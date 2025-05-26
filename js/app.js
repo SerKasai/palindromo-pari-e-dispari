f11d5732ae50276bbd9b2830c9043a517bc714a6
SerKasai <serjtheignis92@gmail.com>
2025-05-26 16:06:46 +0200

    commit 2

--- js/app.js
+++ js/app.js
@@ -23,13 +23,39 @@ console.log(risultato);
 
 // Esercizio pari e dispari
 
-const scelta = prompt('Scegli tra pari o dispari');
+const scelta = prompt('Scegli tra pari o dispari').trim();
 console.log(scelta)
-const userNum = parseInt(prompt('Inserisci un numero'));
-console.log(userNum);
+const userNum = parseInt(prompt('Inserisci un numero da 0 a 5'));
+console.log(`Il numero scelto da te è ${userNum}`);
 
 function randomBotNum(max) {
   return Math.floor(Math.random() * max);
 }
+const botNum = randomBotNum(6)
+console.log(`Il bot sceglie ${botNum}`);
 
-console.log(randomBotNum(6));
+const somma = (userNum + botNum)
+console.log(`La somma dei due numeri è ${somma}`)
+
+function pariOdispari() {
+  let pari = true
+  if (somma % 2 !== 0) {
+    pari = false
+    console.log('Il numero è dispari')
+  } else {
+    console.log('Il numero è pari')
+  }
+  return pari
+
+}
+
+let ver = pariOdispari(somma)
+console.log(ver)
+
+if ((scelta === 'pari') && (ver = true)) {
+  console.log('Hai scelto pari e hai vinto!')
+} else if ((scelta === 'dispari') && (ver = false)) {
+  console.log('Hai scelto dispari e hai vinto!')
+} else {
+  console.log('Hai perso')
+}
